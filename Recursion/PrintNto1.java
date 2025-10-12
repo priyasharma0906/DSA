c2539a90c66435f603d627144f9618c0ea02b0cf
priyasharma0906 <122222524+priyasharma0906@users.noreply.github.com>
2025-10-13 01:07:53 +0530

    Update PrintNto1.java

--- Recursion/PrintNto1.java
+++ Recursion/PrintNto1.java
@@ -3,16 +3,19 @@ package Recursion;
 import java.util.Scanner;
 
 public class PrintNto1 {
-    //Print numbers from 1 to n using recursion
+    //Print numbers from n to 1 using recursion
     public static void printNum(int i, int n){
-        if(i<1) return;
+        if(i>n) return;
+        printNum(i+1, n);
         System.out.println(i);
-        printNum(i-1,n);
+        
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number till you want to print");
+        System.out.println("Enter  the number");
+        int i = 1;
         int n = sc.nextInt();
-        printNum(n, n);
+        printNum(i,n);
     }
+
 }
